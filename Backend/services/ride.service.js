fe2e47778bfcb771fad49bbb8ae79379e6057004
adwaid-p <adwaidpaloli007@gmail.com>
2025-01-14 14:14:03 +0530

    after creating route for ride

---
+++ Backend/services/ride.service.js
@@ -0,0 +1,58 @@
+const rideModel = require('../models/ride.model')
+const mapService = require('../services/maps.service')
+const crypto = require('crypto');
+
+async function getFare(pickup,destination){
+    if(!pickup || !destination){
+        throw new Error('Pickup and destination are required')
+    }
+
+    const distanceTime = await mapService.getDistaceTime(pickup, destination)
+
+    const baseFare = {
+        auto: 20,
+        car: 50,
+        motorcycle: 15
+    }
+
+    const perKmRate = {
+        auto: 10,
+        car: 20,
+        motorcycle: 8
+    }
+
+    const perMinuteRate = {
+        auto: 1,
+        car: 2,
+        motorcycle: 0.5
+    }
+
+    const fare = {
+        auto: baseFare.auto + ((distanceTime.distance.value)/1000 * perKmRate.auto) + ((distanceTime.duration.value)/60 * perMinuteRate.auto),
+        car: baseFare.car + ((distanceTime.distance.value)/1000 * perKmRate.car) + ((distanceTime.duration.value)/60 * perMinuteRate.car),
+        motorcycle: baseFare.motorcycle + ((distanceTime.distance.value)/1000 * perKmRate.motorcycle) + ((distanceTime.duration.value)/60 * perMinuteRate.motorcycle)
+    }
+
+    return fare
+}
+
+function getOtp(num){
+    const otp = crypto.randomInt(Math.pow(10, num - 1), Math.pow(10, num)).toString();
+    return otp;
+}
+
+module.exports.createRide = async({user, pickup, destination, vehicleType})=>{
+    if(!user || !pickup || !destination || !vehicleType){
+        throw new Error('All fields are required')
+    }
+
+    const fare = await getFare(pickup,destination)
+    const ride = rideModel.create({
+        user,
+        pickup,
+        destination,
+        otp: getOtp(6),
+        fare: fare[vehicleType]
+    })
+    return ride
+}
